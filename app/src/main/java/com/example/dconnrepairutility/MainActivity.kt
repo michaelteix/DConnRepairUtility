acdfb6869c9af2b86157f52558cf14c63222adee
Michael Teixeira <michael.teixei@gmail.com>
2020-01-27 18:46:05 -0600

    Remove utility_daemon class and move code to main activity. Add event handeling for start and stop buttons. Add functionality for checking connection speed and name.

--- app/src/main/java/com/example/dconnrepairutility/MainActivity.kt
+++ app/src/main/java/com/example/dconnrepairutility/MainActivity.kt
@@ -1,24 +1,80 @@
 package com.example.dconnrepairutility
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.dconnrepairutility.UtilityViewModel.UtilityVM.StartDaemon
-import com.example.dconnrepairutility.UtilityViewModel.UtilityVM.StopDaemon
+import android.util.Log.d
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+    var lastConnectionName = "None"
+    var lastConnectionSpeed = 0
+    var isActive = false
+    var timeInterval = 30
+    var downloadThreshold = 2
+    private var job: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val utilityVM = UtilityViewModel()
+
+        StopDaemonButton.isEnabled = false
+        LastConnectionValue.setText("Null")
+        DownSpeedValue.setText("Null")
+        TimeIntervalValue.setText(timeInterval.toString())
+        ThresholdValue.setText(downloadThreshold.toString())
 
         StartDaemonButton.setOnClickListener {
-            utilityVM.run { StartDaemon() }
+            isActive = true
+            isDaemonActiveLight.buttonTintList = getColorStateList(R.color.colorGreenStart)
+            StartDaemonButton.isEnabled = false
+            StopDaemonButton.isEnabled = true
+
+            job = GlobalScope.launch(Dispatchers.IO) {
+                d("daemon", "Daemon started with ThreadId: " + Thread.currentThread().id)
+                d("daemon", "Daemon started with JobId: " + job!!.key)
+                val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+                var networkCapabilities: NetworkCapabilities?
+
+                while (true)
+                {
+                    // Check connection speed
+                    networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+                    lastConnectionName = networkCapabilities.toString()
+                    lastConnectionSpeed = networkCapabilities.linkDownstreamBandwidthKbps / 1000
+                    d("daemon", "Tested connection to: " + lastConnectionName)
+                    d("daemon", "Speed: " + lastConnectionSpeed)
+                    //var upSpeed = nc.linkUpstreamBandwidthKbps
+                    try {
+                        LastConnectionValue.setText(lastConnectionName)
+                    }
+                    catch (e: Exception)
+                    {
+                        d("daemon", "Could not write string: " + lastConnectionName)
+                    }
+                    DownSpeedValue.setText(lastConnectionSpeed.toString())
+
+                    // If connection below the threshold run repair service
+                    if(lastConnectionSpeed < downloadThreshold * 1000)
+                    {
+                        // Open dialer and run code *#*#34963#*#*
+                            // Alternatively run repair service and grab statistics from it
+                    }
+
+                    delay(timeInterval.toLong() * 60000)
+                }
+            }
         }
 
         StopDaemonButton.setOnClickListener {
-            utilityVM.run { StopDaemon() }
+            isActive = false
+            StopDaemonButton.isEnabled = false
+            StartDaemonButton.isEnabled = true
+            isDaemonActiveLight.buttonTintList = getColorStateList(R.color.colorRedStatusLight)
+            job!!.cancel()
         }
 
         LogFileButton.setOnClickListener {
